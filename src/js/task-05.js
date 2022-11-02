73dc05ba56baea5bef6a59c679e6120ba6bbc459
win0rdie <rostyslavhnatovskyi@gmail.com>
2022-11-02 22:29:39 +0200

    done 5task

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,14 +1,15 @@
-// Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input), 
-// підставляє його поточне значення в span#name-output. Якщо інпут порожній, у спані 
+// Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input),
+// підставляє його поточне значення в span#name-output. Якщо інпут порожній, у спані
 // повинен відображатися рядок "Anonymous".
 
 // <input type="text" id="name-input" placeholder="Please enter your name" />
 // <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
 
-const output = document.getElementById('name-output');
-const inputText = document.getElementById('name-input'); 
+const output = document.getElementById("name-output");
+const inputText = document.getElementById("name-input");
 
 inputText.addEventListener("input", (event) => {
-    output.textContent = event.currentTarget.value;
-  });
-
+  event.currentTarget.value === ""
+    ? (output.textContent = "Anonymous")
+    : (output.textContent = event.currentTarget.value);
+});
